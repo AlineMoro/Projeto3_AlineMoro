c020fa030bb84b388c1640dbdd73eb312f0e2ec1
Aline Moro <aline_licota@hotmail.com>
2021-08-29 12:51:57 -0400

    conexão com o mongo

--- express.js
+++ express.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
-const mongo_url = 'mongodb://localhost:27017/teste3';
+const mongo_url = 'mongodb+srv://deploy:deploy123@cluster0.a0b38.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const upload = multer({dest: './uploads'});
 
 mongoose.connect(mongo_url, function(err) {
